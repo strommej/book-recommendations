f79919c3e4a956f176e15620633a30dbccd648bf
Joshua Stromme <joshua.stromme@nike.com>
2025-09-29 11:59:24 -0700

    remove hello resolvers

--- api/src/resolvers/index.ts
+++ api/src/resolvers/index.ts
@@ -1,11 +1,9 @@
-import { helloResolvers } from "./hello/index.js";
 import { likesResolvers } from "./likes/index.js";
 import { recommendationsResolvers } from "./recommendations/index.js";
 
 // Combine all resolvers
 export const resolvers = {
   Query: {
-    ...helloResolvers.Query,
     ...recommendationsResolvers.Query,
     ...likesResolvers.Query
   },
